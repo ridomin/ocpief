29bd6c2420e3b77baf19b7ed7ab3a89bec2b0e37
ridomin <rmpablos@hotmail.com>
2016-07-06 22:48:48 -0700

    eslint

--- index.js
+++ index.js
@@ -1,97 +1,95 @@
 /* global __dirname */
-var fs = require('fs');
-var path = require('path');
-var DOMParser = require('xmldom').DOMParser;
-var XMLSerializer = require('xmldom').XMLSerializer;
-var ai = require('applicationinsights');
+var fs = require('fs')
+var path = require('path')
+var DOMParser = require('xmldom').DOMParser
+var XMLSerializer = require('xmldom').XMLSerializer
+var ai = require('applicationinsights')
 
-console.log("OCPIEF 0.0.4-beta")
-console.log("Create a Cordova Project in Existing folder \n\n")
+console.log('OCPIEF 0.0.4-beta')
+console.log('Create a Cordova Project in Existing folder \n\n')
 
+var client = ai.getClient('52df4438-1324-41d1-80ce-658c182f18b5')
 
-var client = ai.getClient("52df4438-1324-41d1-80ce-658c182f18b5");
+var projectFile = getFolderName() + '.jsproj'
 
-var projectFile = getFolderName() + '.jsproj';
+addWin10Preference()
 
-addWin10Preference();
-
-if (fs.existsSync(projectFile)){
-    console.log("Project File found. " +projectFile);
-    openProject(projectFile);
-    client.trackEvent("project opened")
+if (fs.existsSync(projectFile)) {
+  console.log('Project File found. ' + projectFile)
+  openProject(projectFile)
+  client.trackEvent('project opened')
 } else {
-    console.log("Creating Default Cordova VS Project. " + projectFile)
-    initAndOpenCordovaProject(projectFile);
-    client.trackEvent("project created")    
+  console.log('Creating Default Cordova VS Project. ' + projectFile)
+  initAndOpenCordovaProject(projectFile)
+  client.trackEvent('project created')
 }
 
-function initAndOpenCordovaProject(projectFile){
-    fs.writeFile('taco.json', '{"cordova-cli":"6.2.0"}', function(err){if(err) throw err;});               
-    fs.createReadStream(path.resolve(__dirname,'JSPROJTEMPLATE.jsproj'))
+function initAndOpenCordovaProject (projectFile) {
+  fs.writeFile('taco.json', '{"cordova-cli":"6.2.0"}', function (err) { if (err) throw err })
+  fs.createReadStream(path.resolve(__dirname, 'JSPROJTEMPLATE.jsproj'))
             .pipe(fs.createWriteStream(projectFile)
-                .on('finish', function() {
-                    openProject(projectFile);                                              
+                .on('finish', function () {
+                  openProject(projectFile)
                 })
-            );
+            )
 }
 
-function addWin10Preference(){
-    
-    fs.readFile('config.xml', 'utf8', function(err, data){
-        var doc = new DOMParser().parseFromString(data);
-        var prefs = doc.getElementsByTagName("preference");               
-        var found = false;
-        for (var p in prefs){
-            var nameAttribute = prefs[p].attributes;
-            //if found node
-            if (nameAttribute && nameAttribute[0] && nameAttribute[0].name==="name" && nameAttribute[0].value==='windows-target-version'){
-                found = true;   
-                console.log("found config.xml/preference")             
-                //if found value
-                if (nameAttribute && nameAttribute[1] && nameAttribute[1].name==="value" && nameAttribute[1].value==='10.0'){
-                    console.log("already on windows 10")
-                    return;
-                } else {
-                    console.log("upgrading to 10")
-                    nameAttribute[1].value = '10.0'                                        
-                } 
-            } 
-                                        
-        }
-        
-        var widget = doc.getElementsByTagName("widget")[0];
-        
-        if (!found){
-            console.log("node win prefs not found.. creating node")
-            var newPref = doc.createElement("preference")            
-            newPref.setAttribute('name','windows-target-version');            
-            newPref.setAttribute('value','10.0');            
-            widget.appendChild(newPref);            
-        }
-        
-        var serializer = new XMLSerializer();
-        var newConfigXml = serializer.serializeToString(doc)
-        fs.writeFile('config.xml', newConfigXml, 'utf8', function(err, data){
-            if (err) return console.log(err);
-        });
-                                   
-    });     
-}
-
-
-function openProject(projFile){
-    const exec = require('child_process').exec;
-    exec(projFile, function(err, stdout, stderr) {
+function addWin10Preference () {
+  fs.readFile('config.xml', 'utf8', function (err, data) {
     if (err) {
-        console.error(err);
-        return;
+      console.error(err)
+      return
     }
-    console.log(stdout);
-    });
+    var doc = new DOMParser().parseFromString(data)
+    var prefs = doc.getElementsByTagName('preference')
+    var found = false
+    for (var p in prefs) {
+      var nameAttribute = prefs[p].attributes
+      // if found node
+      if (nameAttribute && nameAttribute[0] && nameAttribute[0].name === 'name' && nameAttribute[0].value === 'windows-target-version') {
+        found = true
+        console.log('found config.xml/preference')
+                // if found value
+        if (nameAttribute && nameAttribute[1] && nameAttribute[1].name === 'value' && nameAttribute[1].value === '10.0') {
+          console.log('already on windows 10')
+          return
+        } else {
+          console.log('upgrading to 10')
+          nameAttribute[1].value = '10.0'
+        }
+      }
+    }
 
+    var widget = doc.getElementsByTagName('widget')[0]
+
+    if (!found) {
+      console.log('node win prefs not found.. creating node')
+      var newPref = doc.createElement('preference')
+      newPref.setAttribute('name', 'windows-target-version')
+      newPref.setAttribute('value', '10.0')
+      widget.appendChild(newPref)
+    }
+
+    var serializer = new XMLSerializer()
+    var newConfigXml = serializer.serializeToString(doc)
+    fs.writeFile('config.xml', newConfigXml, 'utf8', function (err, data) {
+      if (err) return console.log(err)
+    })
+  })
 }
 
-function getFolderName(){
-    var ap = path.resolve('.').split(path.sep);
-    return ap[ap.length-1];    
+function openProject (projFile) {
+  const exec = require('child_process').exec
+  exec(projFile, function (err, stdout, stderr) {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(stdout)
+  })
+}
+
+function getFolderName () {
+  var ap = path.resolve('.').split(path.sep)
+  return ap[ap.length - 1]
 }
